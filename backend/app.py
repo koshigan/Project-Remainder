fdeb0ad95f8cf3b2e238660db6b28538b657af16
koshigan <koshigan23@gmail.com>
2026-06-22 04:14:29 +0530

    commit from koshi

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def home():
-    return {"message": "Backend is running"}
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
