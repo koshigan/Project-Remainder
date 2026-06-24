86f1bd67fce2d8a080a09b672716dea8553169ba
koshigan <koshigan23@gmail.com>
2026-06-24 22:24:44 +0530

    edited in app.py

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,6 @@ from reminder import reminder_bp
 
 app = Flask(__name__)
 CORS(app)
-
-# Email Configuration
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
